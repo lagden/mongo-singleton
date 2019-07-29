81494027ba3d965a7688aadba0d5372e04e9eafc
Thiago Lagden <lagden@gmail.com>
2019-07-29 16:14:03 -0300

    add listDb in test

--- test/index.js
+++ test/index.js
@@ -14,7 +14,18 @@ test.after(async () => {
 	await mongod.stop()
 })
 
-test('basic', async t => {
+test('db', async t => {
+	const mongoConn = await mongod.getConnectionString()
+	const mongoDB = await mongod.getDbName()
+	const client = await Mongo.conn(mongoConn)
+	const db = await client.db(mongoDB, {noListener: true, returnNonCachedInstance: true})
+	const admin = db.admin()
+	const {databases} = await admin.listDatabases({nameOnly: true})
+	t.true(Array.isArray(databases))
+	t.is(databases.length, 3)
+})
+
+test('collection', async t => {
 	const mongoConn = await mongod.getConnectionString()
 	const mongoDB = await mongod.getDbName()
 	await Mongo.conn(mongoConn)
